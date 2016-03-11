fee9cada4c0b46b9f6d5d204f80258dd785c37f4
Jake Trigg <jaketrigg@gmail.com>
2016-03-11 16:18:19 +0000

    concept changed to recieve vectors

--- ProgramFiles/AI/AI WA/Concept.cpp
+++ ProgramFiles/AI/AI WA/Concept.cpp
@@ -1,13 +1,16 @@
 #include "Concept.h"
+#include "File.h"
 
-
+// vector<type>&name
 
 Concept::Concept(char* a1)
 {
 	int* a2 = 0;	// The Value of the #G in the source file; Amount of participants given a specific cue.
 	int* a3 = 0;	// The Value of the #P in the source file; Amount of participants that gave the specific response to the specific cue.
 	
-	// Call File::getConcept(a1, a2, a3)
+	// Call File::getConcept(vector<vector<char*>*>& vec);
+	File* f;
+	//f->getConcept( vector<vector<char*>*>& vec);
 	
 	conceptWord = a1;
 	gArray = a2;
