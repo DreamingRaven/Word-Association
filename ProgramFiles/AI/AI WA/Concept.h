579a5514c8c95e954ab8ea78a8ebaba62f42005c
Khaldir <jaketrigg@gmail.com>
2016-03-25 22:41:10 +0000

    Version 1 of the Concept Class

--- ProgramFiles/AI/AI WA/Concept.h
+++ ProgramFiles/AI/AI WA/Concept.h
@@ -1,16 +1,19 @@
 #pragma once
+#include "File.h"
+#include <string>
+#include <vector>
 class Concept
 {
 private:
-	char* conceptWord;
-	int* gArray;
-	int* pArray;
-	// hey (its G) you may want to check get/setConcept
-	// as its changed types, to make it easier.
-	void getWordData(char* word, int* fsg);
-	int* generateFSG();
+	std::string conceptWord;
+	std::vector<std::vector<std::string>> concept;
+	int cueFrequency;
+	float Concept::getFSG(std::vector<std::string> data, int cueFrequency);
+	int getFreq(std::vector<std::string> data);
+	std::string Concept::getWord(std::vector<std::string> data);
 public:
-	Concept(char* a1);
+	Concept(std::string word, File fileObject);
 	~Concept();
+	int Concept::getWordData(int index, std::string* word, float* fsg);
 };
 
