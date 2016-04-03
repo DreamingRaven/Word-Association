67bcf1509556e08287f21ae2a8e23f710df6c84a
Raven <DreamingRaven@users.noreply.github.com>
2016-04-03 18:03:26 +0100

    nulled test code

--- ProgramFiles/AI/AI WA/main.cpp
+++ ProgramFiles/AI/AI WA/main.cpp
@@ -9,20 +9,20 @@ int main(int argc, char** argv)
 	// the testing zone.
 	std::vector<std::vector<std::string> > testMultiDimensionVector;
 	std::cout << "\npath = " << argv[0] << "\n" << std::endl;
-	std::string testString = "AARDVARK";
+	std::string testString = "STREETTT";
 
 
 	// Test of calling Persona
-	Persona A;
-	std::cout << (A.getBestWord(testString)) << std::endl;
-	A.~Persona();
+	//Persona A;
+	//std::cout << (A.getBestWord(testString)) << std::endl;
+	//A.~Persona();
 
 	// Nigh error proof File calls
-	File G("..\\..\\..\\ProgramResources\\G Appendix\\G A-Z.txt");
-	testMultiDimensionVector = G.getConcept(testString);
+	//File G("..\\..\\..\\ProgramResources\\G Appendix\\G A-Z.txt");
+	//testMultiDimensionVector = G.getConcept(testString);
 	
 	// Concept calls (needs work in getWordData)
-	Concept J(testString, G);
+	//Concept J(testString, G);
 	//J.getWordData();
 			
 	return 0;
