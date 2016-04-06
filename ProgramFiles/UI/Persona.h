f95cc7e805f0c9c2844addaa2495a350a6513c64
Jake Trigg <jaketrigg@googlemail.com>
2016-04-06 10:57:04 +0100

    Manual Merge of TuringWork

---
+++ ProgramFiles/UI/Persona.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream> // you must have this to call std::
+#include "File.h"
+class Persona
+{
+private:
+	File* fileObject;
+	std::string bestWord;
+	void getOutputWord();
+	std::vector<std::string> relatedWords;
+	//std::string relatedWords[30];
+	std::vector<float> relatedFSG;
+	//float relatedFSG[30];
+	std::vector<std::string> personaWords;
+	//std::string personaWords[30];
+	std::vector<float> personaFSG;
+	//float personaFSG[30];
+	void generatePersona(std::string* name, std::string* age, std::string* job);
+	std::string personaUsing;
+	std::string personaFactors[2];
+	std::string personaName;
+	std::string personaAge;
+	std::string personaJob;
+public:
+	Persona(std::string* name, std::string* age, std::string* job);
+	~Persona();
+	std::string getBestWord(std::string word);
+};
+
